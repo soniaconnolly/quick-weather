4eda6ee785d022ecd5081e5f1f18fe852b78e583
Sonia Connolly <2381438+soniaconnolly@users.noreply.github.com>
2024-09-07 17:45:28 -0700

    Do not restrict just to modern browsers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # Uncomment to only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  # This is a simple app, so allow all versions.
+  # allow_browser versions: :modern
 end
