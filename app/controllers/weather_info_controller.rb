735477722b9b1d3f6b3196247fac448031e9bda5
Sonia Connolly <2381438+soniaconnolly@users.noreply.github.com>
2024-09-07 17:45:28 -0700

    Add show view and fill in show action

--- app/controllers/weather_info_controller.rb
+++ app/controllers/weather_info_controller.rb
@@ -1,4 +1,16 @@
 class WeatherInfoController < ApplicationController
   def show
+    @address = params['address']
+
+    begin
+      if @address
+        position = GeocodeService.call(@address)
+        if position
+          @weather = WeatherService.call(lat: position[:lat], lon: position[:lon])
+        end
+      end
+    rescue => e
+      flash.now['alert'] = e.message
+    end
   end
 end
--- spec/views/weather_info/show.html.erb_spec.rb
+++ spec/views/weather_info/show.html.erb_spec.rb
@@ -1,5 +1,55 @@
 require 'rails_helper'
 
-RSpec.describe "weather_info/show.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'weather_info/show.html.erb', type: :view do
+  context 'no params' do
+    it 'displays an address field' do
+      render
+
+      expect(rendered).to include I18n.t('weather_info.labels.address')
+      expect(rendered).to include '<input type="text" name="address" id="address" size="70" />'
+    end
+
+    it 'displays the heading' do
+      render
+
+      expect(rendered).to include I18n.t('weather_info.heading')
+    end
+  end
+
+  context '@weather has values' do
+    let(:temp) { 60 }
+    let(:temp_min) { 50 }
+    let(:temp_max) { 70 }
+    let(:cached) { false }
+
+    before do
+      assign(:weather, { temp: temp, temp_min: temp_min, temp_max: temp_max, cached: cached} )
+
+      render
+    end
+
+    it 'displays the current temperature' do
+      expect(rendered).to include I18n.t('weather_info.labels.current_temp', temp: temp)
+    end
+
+    it 'displays the min temperature' do
+      expect(rendered).to include I18n.t('weather_info.labels.min_temp', temp: temp_min)
+    end
+
+    it 'displays the current temperature' do
+      expect(rendered).to include I18n.t('weather_info.labels.max_temp', temp: temp_max)
+    end
+
+    it 'displays the not_cached message' do
+      expect(rendered).to include I18n.t('weather_info.labels.not_cached')
+    end
+
+    context 'cached is true' do
+      let(:cached) { true }
+
+      it 'displays the cached message' do
+        expect(rendered).to include I18n.t('weather_info.labels.cached')
+      end
+    end
+  end
 end
